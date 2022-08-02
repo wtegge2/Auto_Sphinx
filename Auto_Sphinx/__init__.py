8bbebbbbe8a6c3061f00e998190b2b8a8e85aee4
wtegge2 <80014792+wtegge2@users.noreply.github.com>
2022-08-02 12:29:25 -0500

    added package stuff

---
+++ Auto_Sphinx/__init__.py
@@ -0,0 +1,6 @@
+from tasks import answers
+import os
+
+os.system("invoke answers")
+
+
--- setup.py
+++ setup.py
@@ -1,1 +1,26 @@
-from tasks import answers
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Automatically calls Sphinx'
+
+# Setting up
+setup(
+    name="Auto_Sphinx_willyt123",
+    version=VERSION,
+    author="Will",
+    author_email="<teggew@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
